4a212d0b8f83af7cbbef508d919baf01ef15e444
Mahitha-pasupuleti <mahithapasupuleti23@gmail.com>
2024-06-18 17:46:38 -0700

    all configurations completed

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,10 +1,8 @@
 const asyncHandler = (requestHanlder) => {
-    (req, res, next) => {
+    return (req, res, next) => {
         Promise.resolve(requestHanlder(req, res, next)).catch((err) => next(err))
     }
 }
-
-
 
 export {asyncHandler}
 
