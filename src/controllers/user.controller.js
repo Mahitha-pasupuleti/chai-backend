b157bc62c24551d6fbe89ca299afe4b6a2b0e0df
Mahitha-pasupuleti <mahithapasupuleti23@gmail.com>
2024-06-20 22:17:19 -0700

    user registration testing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -19,7 +19,7 @@ const registerUser = asyncHandler( async (req, res) => {
     */
 
     const {fullName, email, username, password} = req.body
-    console.log("email: ", email);
+    console.log("email: ", fullName);
 
     // if(fullName === "") {
     //     throw new ApiError(400, "fullname is required");
@@ -31,19 +31,24 @@ const registerUser = asyncHandler( async (req, res) => {
         throw new ApiError(400, "All fields are required");
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         $or: [{ username }, { email }]
     })
 
     if (existedUser) {
         throw new ApiError(409, "User with email or username already exists")
     }
-
+    
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
 
-    if(!avatarLocalPath) {
+    if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required");
+    }
+
+    let coverImageLocalPath;
+    if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) {
+        coverImageLocalPath = req.files.coverImage[0].path
     }
 
     const avatar = await uploadOnCloudinary(avatarLocalPath)
